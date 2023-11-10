3ec34cb3f5e787cfb3337dafcaabc09b5a98fa1c
Pier Luc Ducharme <dutch80@gmail.com>
2023-11-10 08:38:41 -0500

    Ajout exemple Consumer et Producer Kafka

---
+++ Kafka-Docker/Exemples C#/Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:29092",
+                GroupId = "test-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            consumer.Subscribe("quickstart-events");
+            try
+            {
+                while (true)
+                {
+                    var message = consumer.Consume();
+                    Console.WriteLine($"Received message: {message.Message.Value}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Kafka-Docker/Exemples C#/Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:29092"
+
+            };
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            var topic = "quickstart-events";
+            var message = new Message<Null, string> { Value = "Hello, Kafka!" };
+            producer.Produce(topic, message, deliveryReport => {
+                Console.WriteLine(deliveryReport.Message.Value);
+            });
+            producer.Flush();
+        }
+    }
+}
